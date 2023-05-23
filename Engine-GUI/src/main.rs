d77f07deea7d587979af877fc80f91403b345be3
Pyro <79991107+Pyro569@users.noreply.github.com>
2023-05-22 22:10:49 -0400

    Update main.rs

--- Engine-GUI/src/main.rs
+++ Engine-GUI/src/main.rs
@@ -1,8 +1,25 @@
 use druid::widget::Label;
+use druid::widget::{Container, Flex, Split};
+use druid::Color;
 use druid::{AppLauncher, Widget, WindowDesc};
 
 fn build_ui() -> impl Widget<()> {
-    Label::new("KobiWare Engine GUI")
+    Split::columns(
+        Container::new(
+            Flex::column()
+                .with_flex_child(Label::new("2D"), 1.0)
+                .with_flex_child(Label::new("Raycaster"), 1.0)
+                .with_flex_child(Label::new("3D"), 1.0)
+                .with_flex_child(Label::new("Build"), 1.0),
+        )
+        .border(Color::grey(0.6), 2.0),
+        Container::new(
+            Flex::column()
+                .with_flex_child(Label::new("Button placeholder"), 1.0)
+                .with_flex_child(Label::new("Textbox placeholder"), 1.0),
+        )
+        .border(Color::grey(0.6), 2.0),
+    )
 }
 
 fn main() {
